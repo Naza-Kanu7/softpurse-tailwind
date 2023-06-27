95f00e24b97ddc5d7b256ad0c175e044f68f2290
Naza Kanu <kanuchinaza70@gmail.com>
2023-06-27 11:10:12 +0100

    Fourth Commit

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -10,10 +10,10 @@ function Banner() {
             <div className='flex flex-col md:pt-[3rem] 2xl:pt-[5rem]'>
                 <h1 className='text-[30px] font-bold pb-4 sm:text-[35px] lg:text-[40px] 2xl:text-[50px]'>Providing Solutions, and Enriching You</h1>
                 <p className='text-[24px] sm:text-[28px] lg:text-[32px] 2xl:text-[40px]'>The best app to increase your productivity, giving your craft online presence.</p>
-                <div className='flex 2sm:flex-col flex-row items-start gap-3 pt-4 sm:flex-row sm:pt-7 lg:gap-11 lg:pt-6 xl:pt-10 2xl:pt-12'>
+                <div className='flex 2sm:flex-col flex-row items-center justify-center gap-3 pt-4 sm:flex-row sm:items-center sm:pt-7 lg:gap-11 lg:pt-6 xl:pt-10 2xl:pt-12'>
                     
-                    <button className='bg-banner-button text-whit 2sm:px-7 px-4 py-3 sm:px-9 sm:py-5 rounded-[10px] text-[17px] md:w-[170px] xl:w-[15rem]'><Link to="faq" smooth={true} offset={-200} duration={500}>Learn More</Link></button>
-                    <div className='w-[9rem] 2sm:w-[9.7rem] h-[3.1rem] rounded-[10px] cursor-pointer sm:w-[10rem] sm:h-[4.1rem] md:w-[10rem] lg:w-[12rem] xl:w-[14rem]'>
+                    {/* <button className='bg-banner-button text-whit 2sm:px-7 px-4 py-3 sm:px-9 sm:py-5 rounded-[10px] text-[17px] md:w-[170px] xl:w-[15rem]'><Link to="faq" smooth={true} offset={-200} duration={500}>Learn More</Link></button> */}
+                    <div className='w-[9rem] 2sm:w-[9.7rem] h-[3.1rem] rounded-[10px] cursor-pointer sm:w-[13rem] sm:h-[4rem] md:w-[12rem] lg:w-[12rem] xl:w-[14rem]'>
                         <img src={googlePlayImg} alt="google-play-img" className='w-full h-full rounded-[10px]' />
                     </div>
                 </div>
--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -9,25 +9,25 @@ import Twitter from './Twitter.png'
 function Footer() {
   return (
     <div name='footer' className='w-full bg-footer-bg py-[4rem] flex flex-col items-center gap-[2rem]'>
-        <div className='flex flex-col gap-[0.7rem] items-center md:flex-row'>
-            <div className='w-[50%] sm:w-[65%]'>
+        <div className='flex flex-col gap-[0.9rem] items-center md:flex-row md:pl-[6rem]'>
+            <div className='w-[50%] sm:w-[40%]'>
                 <img src={FooterLogo} alt="" className='w-full h-full' />
             </div>
-            <div className='w-[50%] sm:w-[65%]'>
+            <div className='w-[50%] sm:w-[40%]'>
                 <img src={GooglePlay} alt="" className='w-full h-full' />
             </div>
         </div>
-        <div className='w-[55%] flex flex-row gap-5 sm:gap-7 md:gap-9 lg:w-[40%] xl:w-[30%] 2xl:w-[20%]'>
-            <div className='w-[16%] h-[2.5rem] sm:w-[20%] sm:h-[3.5rem] md:w-[25%] md:h-[4.5rem] cursor-pointer'>
+        <div className='w-[55%] flex flex-row gap-5 sm:gap-7 items-center md:gap-9 lg:w-[40%] xl:w-[30%] 2xl:w-[20%]'>
+            <div className='w-[16%] h-[2.5rem] sm:w-[20%] sm:h-[3.5rem] md:w-[20%] md:h-[4rem] cursor-pointer'>
                 <a href='https://www.facebook.com/profile.php?id=100077394650393&mibextid=ZbWKwL' target='_blank' rel="noreferrer"><img src={Facebook} alt="facebook" className='w-full h-full'/></a>
             </div>
-            <div className='w-[16%] h-[2.5rem] sm:w-[20%] sm:h-[3.5rem] md:w-[25%] md:h-[4.5rem] cursor-pointer'>
+            <div className='w-[16%] h-[2.5rem] sm:w-[20%] sm:h-[3.5rem] md:w-[20%] md:h-[4rem] cursor-pointer'>
                 <a href='https://wa.link/a78qdh' target='_blank' rel="noreferrer"><img src={Whatsapp} alt="whatsapp" className='w-full h-full'/></a>
             </div>
-            <div className='w-[16%] h-[2.5rem] sm:w-[20%] sm:h-[3.5rem] md:w-[25%] md:h-[4.5rem] cursor-pointer'>
+            <div className='w-[16%] h-[2.5rem] sm:w-[20%] sm:h-[3.5rem] md:w-[20%] md:h-[4rem] cursor-pointer'>
                 <a href='https://twitter.com/techservafrica?t=-IaMPK5wihoW6SzUJkKPqA&s=09' target='_blank' rel="noreferrer"><img src={Twitter} alt="twitter" className='w-full h-full'/></a>
             </div>
-            <div className='w-[16%] h-[2.5rem] sm:w-[20%] sm:h-[3.5rem] md:w-[25%] md:h-[4.5rem] cursor-pointer'>
+            <div className='w-[16%] h-[2.5rem] sm:w-[20%] sm:h-[3.5rem] md:w-[20%] md:h-[4rem] cursor-pointer'>
                 <a href='https://instagram.com/techservafrica?igshid=ZDdkNTZiNTM=' target='_blank' rel="noreferrer"><img src={Instagram} alt="instagram" className='w-full h-full'/></a>
             </div>
         </div>
